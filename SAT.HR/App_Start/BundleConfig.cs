49e50a67824965615cabcd632b1031d175cc9ead
shanisara <shanisara@iconext.co.th>
2018-10-12 10:01:38 +0700

    Update BundleConfig

--- SAT.HR/App_Start/BundleConfig.cs
+++ SAT.HR/App_Start/BundleConfig.cs
@@ -8,36 +8,30 @@ namespace SAT.HR
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Content/assets/js/core/jquery.min.js"));
+                        "~/Content/assets/js/core/jquery.min.js",
+                        "~/Content/assets/js/core/popper.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Content/assets/js/plugins/jquery.validate.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Content/assets/js/core/bootstrap-material-design.min.js",
+                      "~/Content/assets/js/plugins/perfect-scrollbar.jquery.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/plugin").Include(
-                        "~/Content/assets/js/core/popper.min.js",
                         "~/Content/assets/js/plugins/moment.min.js",
-                        "~/Content/assets/js/plugins/perfect-scrollbar.jquery.min.js",
+                        "~/Content/assets/js/plugins/sweetalert2.js",
+                        "~/Content/assets/js/plugins/jquery.bootstrap-wizard.js",
+                        "~/Content/assets/js/plugins/jquery.dataTables.min.js",
                         "~/Content/assets/js/plugins/bootstrap-selectpicker.js",
                         "~/Content/assets/js/plugins/bootstrap-datetimepicker.min.js",
                         "~/Content/assets/js/plugins/bootstrap-notify.js",
                         "~/Content/assets/js/plugins/bootstrap-tagsinput.js",
                         "~/Content/assets/js/plugins/jasny-bootstrap.min.js",
                         "~/Content/assets/js/plugins/nouislider.min.js",
-                        "~/Content/assets/js/plugins/sweetalert2.js",
                         "~/Content/assets/js/plugins/arrive.min.js",
-                        "~/Content/assets/js/plugins/jquery.validate.min.js",
-                        "~/Content/assets/js/plugins/jquery.bootstrap-wizard.js",
-                        "~/Content/assets/js/plugins/jquery.dataTables.min.js",
                         "~/Content/assets/js/plugins/chartist.min.js",
-                        "~/Content/assets/js/core/bootstrap-material-design.min.js",
-                        "~/Content/assets/js/material-dashboard.min.js?v=2.0.2",
+                        "~/Content/assets/js/material-dashboard.min.js",
                         "~/Content/assets/demo/demo.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
