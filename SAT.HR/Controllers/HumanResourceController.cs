1001874077e668790be60b1902b4c078d2e23a02
shan1sara <shan1sara@outlook.com>
2018-11-05 01:27:34 +0700

    update

--- SAT.HR/Controllers/HumanResourceController.cs
+++ SAT.HR/Controllers/HumanResourceController.cs
@@ -1,18 +1,112 @@
-﻿using SAT.HR.Helpers;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SAT.HR.Models;
+using SAT.HR.Data.Repository;
+using SAT.HR.Helpers;
 
 namespace SAT.HR.Controllers
 {
     public class HumanResourceController : BaseController
     {
-        // ระบบพัฒนาบุคลากร
-        #region  บันทึกข้อมูลสมรรถนะ
+        #region สมรรถนะ - Capability
 
         public ActionResult Capability()
+        {
+            return View();
+        }
+
+        public ActionResult CapabilityHeader(int? id)
+        {
+            CapabilityViewModel model = new CapabilityViewModel();
+            if (id.HasValue)
+            {
+                model = new CapabilityRepository().GetByID((int)id);
+            }
+            ViewBag.CapabilityType = DropDownList.GetCapabilityType(model.CapTID);
+            ViewBag.CapabilityGroup = DropDownList.GetCapabilityGroup(model.CapGID);
+            ViewBag.CapabilityGroupType = DropDownList.GetCapabilityGroupType(model.CapGID, model.CapGTID);
+            return PartialView("_Capability", model);
+        }
+
+        public JsonResult CapabilityGroupType(int capgid)
+        {
+            var result = DropDownList.GetCapabilityGroupType(capgid, null);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Capability(int? draw, int? start, int? length, List<Dictionary<string, string>> order, List<Dictionary<string, string>> columns)
+        {
+            var search = Request["search[value]"];
+            var dir = order[0]["dir"].ToLower();
+            var column = columns[int.Parse(order[0]["column"])]["data"];
+            var dataTableData = new CapabilityRepository().GetPage(search, draw, start, length, dir, column);
+            return Json(dataTableData, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult SaveCapability(CapabilityViewModel model)
+        {
+            ResponseData result = new Models.ResponseData();
+            if (model.CapID != 0)
+                result = new CapabilityRepository().UpdateByEntity(model);
+            else
+                result = new CapabilityRepository().AddByEntity(model);
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult DeleteCapability(int id)
+        {
+            var result = new CapabilityRepository().RemoveByID(id);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult CapabilityDetail(int? id)
+        {
+            List<CapabilityDetailViewModel> model = new List<CapabilityDetailViewModel>();
+            if (id.HasValue)
+            {
+                model = new CapabilityDetailRepository().GetByCap((int)id);
+            }
+
+            return PartialView("_CapabilityDetail", model);
+        }
+
+        public JsonResult SaveCapabilityDetail(CapabilityDetailViewModel model)
+        {
+            ResponseData result = new Models.ResponseData();
+            if (model.CapID != 0)
+                result = new CapabilityDetailRepository().UpdateByEntity(model);
+            else
+                result = new CapabilityDetailRepository().AddByEntity(model);
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult SubmitCapabilityDetail(List<CapabilityDetailViewModel> model)
+        {
+            ResponseData result = new Models.ResponseData();
+            result = new CapabilityDetailRepository().SubmitByEntity(model);
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public JsonResult DeleteCapabilityDetail(int id)
+        {
+            var result = new CapabilityDetailRepository().RemoveByID(id);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+
+        #endregion
+        
+        #region  บันทึกข้อมูลสมรรถนะ
+
+        public ActionResult Evaluation()
         {
             return View();
         }
--- SAT.HR/Controllers/MasterController.cs
+++ SAT.HR/Controllers/MasterController.cs
@@ -690,100 +690,7 @@ namespace SAT.HR.Controllers
 
         #endregion
 
-        #region 15. สมรรถนะ - Capability
-
-        public ActionResult Capability()
-        {
-            return View();
-        }
-
-        public ActionResult CapabilityHeader(int? id)
-        {
-            CapabilityViewModel model = new CapabilityViewModel();
-            if (id.HasValue)
-            {
-                model = new CapabilityRepository().GetByID((int)id);
-            }
-            ViewBag.CapabilityType = DropDownList.GetCapabilityType(model.CapTID);
-            ViewBag.CapabilityGroup = DropDownList.GetCapabilityGroup(model.CapGID);
-            ViewBag.CapabilityGroupType = DropDownList.GetCapabilityGroupType(model.CapGID, model.CapGTID);
-            return PartialView("_Capability", model);
-        }
-
-        public JsonResult CapabilityGroupType(int capgid)
-        {
-            var result = DropDownList.GetCapabilityGroupType(capgid, null);
-            return Json(result, JsonRequestBehavior.AllowGet);
-        }
-
-        [HttpPost]
-        public JsonResult Capability(int? draw, int? start, int? length, List<Dictionary<string, string>> order, List<Dictionary<string, string>> columns)
-        {
-            var search = Request["search[value]"];
-            var dir = order[0]["dir"].ToLower();
-            var column = columns[int.Parse(order[0]["column"])]["data"];
-            var dataTableData = new CapabilityRepository().GetPage(search, draw, start, length, dir, column);
-            return Json(dataTableData, JsonRequestBehavior.AllowGet);
-        }
-
-        public JsonResult SaveCapability(CapabilityViewModel model)
-        {
-            ResponseData result = new Models.ResponseData();
-            if (model.CapID != 0)
-                result = new CapabilityRepository().UpdateByEntity(model);
-            else
-                result = new CapabilityRepository().AddByEntity(model);
-
-            return Json(result, JsonRequestBehavior.AllowGet);
-        }
-
-        public JsonResult DeleteCapability(int id)
-        {
-            var result = new CapabilityRepository().RemoveByID(id);
-            return Json(result, JsonRequestBehavior.AllowGet);
-        }
-
-        public ActionResult CapabilityDetail(int? id)
-        {
-            List<CapabilityDetailViewModel> model = new List<CapabilityDetailViewModel>();
-            if (id.HasValue)
-            {
-                model = new CapabilityDetailRepository().GetByCap((int)id);
-            }
-
-            return PartialView("_CapabilityDetail", model);
-        }
-
-        public JsonResult SaveCapabilityDetail(CapabilityDetailViewModel model)
-        {
-            ResponseData result = new Models.ResponseData();
-            if (model.CapID != 0)
-                result = new CapabilityDetailRepository().UpdateByEntity(model);
-            else
-                result = new CapabilityDetailRepository().AddByEntity(model);
-
-            return Json(result, JsonRequestBehavior.AllowGet);
-        }
-
-        public JsonResult SubmitCapabilityDetail(List<CapabilityDetailViewModel> model)
-        {
-            ResponseData result = new Models.ResponseData();
-            result = new CapabilityDetailRepository().SubmitByEntity(model);
-
-            return Json(result, JsonRequestBehavior.AllowGet);
-        }
-
-
-        public JsonResult DeleteCapabilityDetail(int id)
-        {
-            var result = new CapabilityDetailRepository().RemoveByID(id);
-            return Json(result, JsonRequestBehavior.AllowGet);
-        }
-
-
-        #endregion
-
-        #region 16. ประเภทการลา - LeaveType
+        #region 15. ประเภทการลา - LeaveType
 
         public ActionResult LeaveType()
         {
@@ -830,7 +737,7 @@ namespace SAT.HR.Controllers
 
         #endregion
 
-        #region 17. วันหยุด - Holiday
+        #region 16. วันหยุด - Holiday
 
         public ActionResult Holiday()
         {
@@ -876,7 +783,7 @@ namespace SAT.HR.Controllers
 
         #endregion
 
-        #region 18. รหัสการเคลื่อนไหว - ActionType
+        #region 17. รหัสการเคลื่อนไหว - ActionType
 
         public ActionResult ActionType()
         {
@@ -922,7 +829,7 @@ namespace SAT.HR.Controllers
 
         #endregion
 
-        #region 19. ค่าคงที่ระบบ - SystemConfig
+        #region 18. ค่าคงที่ระบบ - SystemConfig
 
         public ActionResult SysConfig()
         {
@@ -940,7 +847,7 @@ namespace SAT.HR.Controllers
 
         #endregion
 
-        #region 20. จัดการสิทธิการเข้าใช้งาน - UserRole
+        #region 19. จัดการสิทธิการเข้าใช้งาน - UserRole
 
         public ActionResult Permission()
         {
@@ -1089,6 +996,7 @@ namespace SAT.HR.Controllers
 
         #endregion
 
+        #region ManPower
 
         public JsonResult GetDetailByUser(int userid)
         {
@@ -1125,5 +1033,7 @@ namespace SAT.HR.Controllers
             var result = DropDownList.GetPositionManPowerValueMp(type, divid, depid, secid, null);
             return Json(result, JsonRequestBehavior.AllowGet);
         }
+
+        #endregion 
     }
 }
