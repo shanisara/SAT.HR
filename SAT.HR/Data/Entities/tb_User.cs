e892e5e878b216a3dbccb836ee997c9360ab806b
shan1sara <shan1sara@outlook.com>
2018-12-14 21:04:36 +0700

    update

--- SAT.HR/Data/Entities/tb_User.cs
+++ SAT.HR/Data/Entities/tb_User.cs
@@ -115,6 +115,7 @@ namespace SAT.HR.Data.Entities
         public string FileAttach2 { get; set; }
         public Nullable<bool> IsTerminate { get; set; }
         public string UserCode { get; set; }
+        public string UserCode1 { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<tb_Benefit_Cremation> tb_Benefit_Cremation { get; set; }
